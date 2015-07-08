ad2b6fe70076996ca5246047e4f760c1b4d85479
Tarek S Hafez <thafez@ymail.com>
2015-07-08 16:34:57 -0500

    Adding .gitignore css/ img/ index.html js/ mariosescapeplan.html

---
+++ js/mariosescape.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var gameLoop;
+var highscore = 0;
+// var endLoop = false;
+
+$(".molehole").click(function (event) {
+	var moleholeClicked = $(this).attr('id');
+	var moleholeCheck = $(this)
+	if (moleholeCheck.hasClass("active")) {
+		highscore += 10;
+		$('#score').text("Score: " + highscore);
+	} else {
+		highscore -= 10;
+		$('#score').text("Score: " + highscore);
+		clearInterval(gameLoop);
+		animateWrongClick(moleholeClicked)
+		gameLoop = setInterval(marioTravels, 1000)
+	}
+});
+
+function animateMario (id) {
+	$('#' + id).addClass('active');
+	setTimeout(function () {
+		$('#' + id).removeClass('active');
+	}, 1000);
+}
+
+function animateWrongClick (id) {
+	$('#' + id).addClass('wrong');
+	var interval = setInterval
+	setTimeout(function () {
+		$('#' + id).removeClass('wrong');
+	}, 250);
+}
+
+function marioTravels () {
+	var squares = $('.molehole');
+	var random = Math.floor(Math.random() * 4);
+	var squareToAnimate = squares[random];
+	var id = squareToAnimate.getAttribute('id');
+	animateMario(id);
+}
+
+$('#play').click(function (e) {
+	e.preventDefault();
+	gameLoop = setInterval(marioTravels, 1000);
+});
